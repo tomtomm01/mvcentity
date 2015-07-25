9ca9b90dc07be3c3b9cfcf8d63e9ff999db55e56
ThomasP <tomtom_01@outlook.com>
2015-07-25 14:07:02 +0200

    25.7. 2

---
+++ mvcentity/ViewModels/EinkaufViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using mvcentity.Models;
+
+namespace mvcentity.ViewModels
+{
+    public class EinkaufViewModel
+    {
+        public string Name { get; set; }
+        public int Anzahl { get; set; }
+        public List<Item> EinkaufListe { get; set; }
+    }
+}
